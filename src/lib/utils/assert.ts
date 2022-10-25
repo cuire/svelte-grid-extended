a9e3a9527e37eb71d130cc6a9bec99d100063ce6
cuire <81014305+cuire@users.noreply.github.com>
2022-10-25 15:01:51 +0500

    ♻️ Fix #2

--- src/lib/utils/assert.ts
+++ src/lib/utils/assert.ts
@@ -11,8 +11,10 @@ export function assertGridOptions(options: GridOptions) {
 	if (
 		(cols === 0 && itemSize?.width === undefined) ||
 		(rows === 0 && itemSize?.height === undefined) ||
-		(typeof cols === 'object' && Object.values(cols).includes(0)) ||
-		(typeof rows === 'object' && Object.values(rows).includes(0) && itemSize === undefined)
+		(typeof cols === 'object' &&
+			Object.values(cols).includes(0) &&
+			itemSize?.width === undefined) ||
+		(typeof rows === 'object' && Object.values(rows).includes(0) && itemSize?.height === undefined)
 	) {
 		throw new Error('If `rows` or `cols` === `0`, the `itemSize` parameter must be specified');
 	}
