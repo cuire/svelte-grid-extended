b83720cff51e316ce7493b9eeb6c017049f927b3
Fabio Traverso <fabio.traversoprado@megatime.cl>
2023-07-23 22:37:59 -0400

    + unit tests and minor changes to match new api

--- tests/unit/grid.test.ts
+++ tests/unit/grid.test.ts
@@ -4,7 +4,8 @@ import {
 	getCollisions,
 	getGridDimensions,
 	hasCollisions,
-	isItemColliding
+	isItemColliding,
+	getAvailablePosition
 } from '../../src/lib/utils/grid';
 
 import type { LayoutItem } from '../../src/lib/types';
@@ -18,14 +19,14 @@ import type { LayoutItem } from '../../src/lib/types';
  * Where ~ is empty spot
  */
 const items: LayoutItem[] = [
-	{ id: '0', x: 0, y: 0, w: 1, h: 1 },
-	{ id: '1', x: 1, y: 0, w: 1, h: 1 },
-	{ id: '2', x: 2, y: 0, w: 1, h: 1 },
-	{ id: '3', x: 3, y: 0, w: 1, h: 1 },
-	{ id: '4', x: 0, y: 1, w: 1, h: 1 },
-	{ id: '5', x: 1, y: 1, w: 3, h: 2 },
-	{ id: '6', x: 0, y: 2, w: 1, h: 1 },
-	{ id: '7', x: 0, y: 3, w: 1, h: 1 }
+	{ id: '0', x: 0, y: 0, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '1', x: 1, y: 0, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '2', x: 2, y: 0, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '3', x: 3, y: 0, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '4', x: 0, y: 1, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '5', x: 1, y: 1, w: 3, h: 2, movable: true, resizable: true },
+	{ id: '6', x: 0, y: 2, w: 1, h: 1, movable: true, resizable: true },
+	{ id: '7', x: 0, y: 3, w: 1, h: 1, movable: true, resizable: true }
 ];
 
 describe('🎏 isItemColliding()', () => {
@@ -40,33 +41,43 @@ describe('🎏 isItemColliding()', () => {
 
 	test.each([
 		[
-			{ id: '5', x: 1, y: 1, w: 3, h: 2 },
-			{ id: '1', x: 1, y: 1, w: 1, h: 1 }
+			{ id: '5', x: 1, y: 1, w: 3, h: 2, movable: true, resizable: true },
+			{ id: '1', x: 1, y: 1, w: 1, h: 1, movable: true, resizable: true }
 		],
 		[
-			{ id: '5', x: 1, y: 1, w: 3, h: 2 },
-			{ id: '1', x: 2, y: 1, w: 1, h: 1 }
+			{ id: '5', x: 1, y: 1, w: 3, h: 2, movable: true, resizable: true },
+			{ id: '1', x: 2, y: 1, w: 1, h: 1, movable: true, resizable: true }
 		],
 		[
-			{ id: '5', x: 1, y: 1, w: 3, h: 2 },
-			{ id: '1', x: 1, y: 2, w: 1, h: 1 }
+			{ id: '5', x: 1, y: 1, w: 3, h: 2, movable: true, resizable: true },
+			{ id: '1', x: 1, y: 2, w: 1, h: 1, movable: true, resizable: true }
 		],
 		[
-			{ id: '5', x: 1, y: 1, w: 3, h: 2 },
-			{ id: '1', x: 2, y: 2, w: 1, h: 1 }
+			{ id: '5', x: 1, y: 1, w: 3, h: 2, movable: true, resizable: true },
+			{ id: '1', x: 2, y: 2, w: 1, h: 1, movable: true, resizable: true }
 		]
 	])("should collide when item is within another item's bounding box", (item1, item2) => {
 		expect(isItemColliding(item1, item2)).toBe(true);
 	});
 
 	test.each(items)("should not collide when item within another item's bounding box", (item) => {
-		const itemTopLeft = { id: 'itemTopLeft', x: item.x - item.w, y: item.y - item.h, w: 1, h: 1 };
+		const itemTopLeft = {
+			id: 'itemTopLeft',
+			x: item.x - item.w,
+			y: item.y - item.h,
+			w: 1,
+			h: 1,
+			movable: true,
+			resizable: true
+		};
 		const itemBottomRight = {
 			id: 'itemBottomRight',
 			x: item.x + item.w,
 			y: item.y + item.h,
 			w: 1,
-			h: 1
+			h: 1,
+			movable: true,
+			resizable: true
 		};
 		expect(isItemColliding(item, itemTopLeft)).toBe(false);
 		expect(isItemColliding(item, itemBottomRight)).toBe(false);
@@ -75,19 +86,19 @@ describe('🎏 isItemColliding()', () => {
 
 describe('🎐 hasCollisions()', () => {
 	test.each([
-		{ id: '8', x: 1, y: 3, w: 1, h: 1 },
-		{ id: '8', x: 2, y: 3, w: 1, h: 1 },
-		{ id: '8', x: 3, y: 3, w: 1, h: 1 },
-		{ id: '8', x: 1, y: 3, w: 2, h: 1 }
+		{ id: '8', x: 1, y: 3, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 2, y: 3, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 3, y: 3, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 1, y: 3, w: 2, h: 1, movable: true, resizable: true }
 	])('should not have collisions', (item) => {
 		expect(hasCollisions(item, items)).toBe(false);
 	});
 
 	test.each([
-		{ id: '8', x: 0, y: 0, w: 1, h: 1 },
-		{ id: '8', x: 0, y: 1, w: 1, h: 1 },
-		{ id: '8', x: 1, y: 0, w: 1, h: 1 },
-		{ id: '8', x: 1, y: 1, w: 1, h: 1 }
+		{ id: '8', x: 0, y: 0, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 0, y: 1, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 1, y: 0, w: 1, h: 1, movable: true, resizable: true },
+		{ id: '8', x: 1, y: 1, w: 1, h: 1, movable: true, resizable: true }
 	])('should have collision', (item) => {
 		expect(hasCollisions(item, items)).toBe(true);
 	});
@@ -95,11 +106,11 @@ describe('🎐 hasCollisions()', () => {
 
 describe('🎑 getCollisions()', () => {
 	test.each([
-		[{ id: '8', x: 0, y: 0, w: 1, h: 1 }, 1],
-		[{ id: '8', x: 0, y: 0, w: 2, h: 1 }, 2],
-		[{ id: '8', x: 0, y: 0, w: 2, h: 2 }, 4],
-		[{ id: '8', x: 0, y: 0, w: 3, h: 3 }, 6],
-		[{ id: '8', x: 0, y: 0, w: 4, h: 4 }, 8]
+		[{ id: '8', x: 0, y: 0, w: 1, h: 1, movable: true, resizable: true }, 1],
+		[{ id: '8', x: 0, y: 0, w: 2, h: 1, movable: true, resizable: true }, 2],
+		[{ id: '8', x: 0, y: 0, w: 2, h: 2, movable: true, resizable: true }, 4],
+		[{ id: '8', x: 0, y: 0, w: 3, h: 3, movable: true, resizable: true }, 6],
+		[{ id: '8', x: 0, y: 0, w: 4, h: 4, movable: true, resizable: true }, 8]
 	])('should have collisions', (item, expected) => {
 		expect(getCollisions(item, items).length).toBe(expected);
 	});
@@ -110,5 +121,42 @@ describe('🍦 getGridDimensions()', () => {
 		const { cols, rows } = getGridDimensions(items);
 		expect(cols).toBe(4);
 		expect(rows).toBe(4);
+	});
+});
+
+describe('🍨 getAvailablePosition()', () => {
+	test.each([
+		{
+			item: { id: '8', x: 0, y: 0, w: 1, h: 1, movable: true, resizable: true },
+			expected: { x: 4, y: 0 },
+			maxCols: 5,
+			maxRows: Infinity
+		},
+		{
+			item: { id: '8', x: 0, y: 0, w: 1, h: 1, movable: true, resizable: true },
+			expected: { x: 1, y: 3 },
+			maxCols: Infinity,
+			maxRows: Infinity
+		},
+		{
+			item: { id: '8', x: 0, y: 0, w: 4, h: 4, movable: true, resizable: true },
+			expected: { x: 0, y: 4 },
+			maxCols: 5,
+			maxRows: Infinity
+		},
+		{
+			item: { id: '8', x: 0, y: 0, w: 4, h: 4, movable: true, resizable: true },
+			expected: { x: 4, y: 0 },
+			maxCols: Infinity,
+			maxRows: 5
+		},
+		{
+			item: { id: '8', x: 0, y: 0, w: 4, h: 4, movable: true, resizable: true },
+			expected: null,
+			maxCols: 5,
+			maxRows: 5
+		}
+	])('should return available position', ({ item, expected, maxCols, maxRows }) => {
+		expect(getAvailablePosition(item, items, maxCols, maxRows)).toEqual(expected);
 	});
 });
